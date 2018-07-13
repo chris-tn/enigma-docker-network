4484430f86be439354caf6ea229143256ac7fb4f
Frederic Fortier <fredfortier@gmail.com>
2018-07-13 16:34:56 -0700

    BLD: breaking up scripts to launch the blockchain

--- 1-launch-blockchain.bash
+++ 1-launch-blockchain.bash
@@ -3,7 +3,19 @@
 ARGF=""
 SRFD=""
 
+echo "Launching the contract service"
 docker-compose up contract
+
+CONTRACT_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' enigma_contract_1)
+while :
+do
+	nc -z $CONTRACT_IP 8545
+	result=$?
+	if [[ $result -eq 0 ]]; then
+        break
+    fi
+    sleep 5
+done
 
 echo "Deploying contracts on testnet..."
 docker-compose exec contract bash -c "rm -rf ~/enigma-contract/build/contracts/*"
