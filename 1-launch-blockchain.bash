7d2543679a847981f30c9771d6403a66de45c716
Frederic Fortier <fredfortier@gmail.com>
2018-07-13 16:39:18 -0700

    BLD: breaking up scripts to launch the blockchain

--- 1-launch-blockchain.bash
+++ 1-launch-blockchain.bash
@@ -20,5 +20,4 @@ done
 
 echo "Deploying contracts on testnet..."
 docker-compose exec contract bash -c "rm -rf ~/enigma-contract/build/contracts/*"
-docker-compose exec contract bash -c "cd enigma-contract && ~/darq-truffle migrate --reset --network ganache"
-docker-compose exec contract bash -c "~/darq-truffle test"
+docker-compose exec contract bash -c "cd enigma-contract && ~/darq-truffle migrate --reset --network ganache && ~/darq-truffle test"
