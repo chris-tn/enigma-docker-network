ec3726f74f88d2113e2cbaf2fc6458bf233979fe
Frederic Fortier <fredfortier@gmail.com>
2018-07-13 17:37:47 -0700

    BLD: broke us the spawn worker script

--- 3-spawn-worker.bash
+++ 3-spawn-worker.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ "$DEVELOP" -eq 1 ]]; then
-    echo "Spawning workers in development mode..."
+    echo "Launching Docker Enigma Network in development mode..."
     ARGF="-f docker-compose.develop.yml"
     SRFD="~/wrapper-develop.bash &&"
 else
@@ -9,9 +9,21 @@ else
     SRFD=""
 fi
 
-docker-compose $ARGF up surface &
+if [ "$1" != "" ]; then
+    NUM_WORKERS=$1
 
-sleep 5
-docker-compose exec surface bash -c "$SRFD ./wait_launch.bash" &
+    if [ $NUM_WORKERS -gt 1 ]; then
+    	for i in $(seq 2 $NUM_WORKERS); do
+            WORKER_INDEX=$i docker-compose $ARGF scale core=$i &
 
+            WORKER_INDEX=$i docker-compose $ARGF scale surface=$i &
+            docker-compose exec --index=$i surface bash -c "$SRFD ./wait_launch.bash; bash" &
 
+    	done
+    fi
+
+else
+	echo "Specify the number of workers with:"
+    echo "./spawn_workers N"
+    echo "where is N is a number greater than 1"
+fi
