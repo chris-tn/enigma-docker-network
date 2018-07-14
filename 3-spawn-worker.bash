986dbea8313ed5b84a7c0a68f6ee9842c083888c
Frederic Fortier <fredfortier@gmail.com>
2018-07-13 19:03:53 -0700

    BLD: mapped hostname to container name

--- 3-spawn-worker.bash
+++ 3-spawn-worker.bash
@@ -11,13 +11,12 @@ fi
 
 if [ "$1" != "" ]; then
     NUM_WORKERS=$1
-    docker-compose $ARGF up --scale surface=$NUM_WORKERS surface &
-    sleep 1
-    docker-compose $ARGF up --scale core=$NUM_WORKERS core &
-    sleep 1
 
     if [ $NUM_WORKERS -gt 1 ]; then
     	for i in $(seq 2 $NUM_WORKERS); do
+            WORKER_INDEX=$i docker-compose $ARGF scale core=$i &
+
+            WORKER_INDEX=$i docker-compose $ARGF scale surface=$i &
             docker-compose exec --index=$i surface bash -c "$SRFD ./wait_launch.bash; bash" &
 
     	done
