452c7900652097c0e33228ae6b80d9faa48113c6
Frederic Fortier <fredfortier@gmail.com>
2018-07-13 17:24:20 -0700

    BLD: broke us the spawn worker script

--- 3-spawn-worker.bash
+++ 3-spawn-worker.bash
@@ -9,23 +9,10 @@ else
     SRFD=""
 fi
 
-if [ "$1" != "" ]; then
-    NUM_WORKERS=$1
+docker-compose $ARGF up --scale core=1 core &
+docker-compose $ARGF up --scale surface=1 surface &
 
-    docker-compose $ARGF up --scale core=$NUM_WORKERS core
-    docker-compose $ARGF up --scale surface=$NUM_WORKERS surface
+sleep 5
+docker-compose exec surface bash -c "$SRFD ./wait_launch.bash" &
 
-    sleep 5
-
-    if [ $NUM_WORKERS -gt 1 ]; then
-    	for i in $(seq 2 $NUM_WORKERS); do
-            docker-compose exec --index=$i surface bash -c "$SRFD ./wait_launch.bash" &
-    	done
-    fi
-
-else
-	echo "Specify the number of workers with:"
-    echo "./spawn_workers N"
-    echo "where is N is a number greater than 1"
-fi
 
